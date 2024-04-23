27f548dc692fcfdd1ad95d24a797b44cf3aafb35
riya <rriya_be20@thapar.edu>
2024-04-23 18:37:14 +0530

    commit 1

---
+++ src/config.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const connect = mongoose.connect("mongodb+srv://user1234:user%401234@cluster0.lwvxean.mongodb.net/");
+
+// Check database connected or not
+
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
